7879b8c11944a696e8a1eba68919027c5bd658c2
Ian Jiwoo Ban <jban365@gmail.com>
2020-04-25 15:27:02 +0900

    Initial Commit

---
+++ Twitter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Twitter
+//
+//  Created by Jiwoo Ban on 2020/04/25.
+//  Copyright © 2020 Jiwoo Ban. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
