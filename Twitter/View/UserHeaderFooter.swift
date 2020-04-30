a5322c0f48a6b14bcaf0da862383b8348cf6f2bf
Ian Jiwoo Ban <jban365@gmail.com>
2020-05-01 01:32:24 +0900

    코드 리펙토링(View에서 UserCell과 UserHeaderFooter구분)

---
+++ Twitter/View/UserHeaderFooter.swift
@@ -0,0 +1,71 @@
+//
+//  Cells.swift
+//  Twitter
+//
+//  Created by Jiwoo Ban on 2020/04/25.
+//  Copyright © 2020 Jiwoo Ban. All rights reserved.
+//
+
+import LBTAComponents
+
+let twitterBlue = UIColor(r: 61, g: 167, b: 244)
+
+//헤더추가하기
+class UserHeader: DatasourceCell {
+    
+//    override var datasourceItem: Any?{
+//        didSet {
+//            headerLabel.text = datasourceItem as? String
+//        }
+//    }
+    
+    let headerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "WHO TO FOLLOW"
+        label.font = UIFont.systemFont(ofSize: 16)
+        return label
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+        
+        //셀을 구분짓는 라인 추가하기
+        separatorLineView.isHidden = false
+//        separatorLineView.backgroundColor = UIColor(r: 230, g: 230, b: 230)
+//        backgroundColor = .blue
+        addSubview(headerLabel)
+        headerLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+        
+    }
+    
+}
+
+//푸터추가하기
+class UserFooter: DatasourceCell {
+    
+//    override var datasourceItem: Any?{
+//        didSet {
+//            footerLabel.text = datasourceItem as? String
+//        }
+//    }
+    
+    let footerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Show me more"
+        label.font = UIFont.systemFont(ofSize: 15)
+        label.textColor = twitterBlue
+        return label
+    }()
+    
+    override func setupViews() {
+        super.setupViews()
+//        backgroundColor = .systemPink
+        
+        addSubview(footerLabel)
+        footerLabel.anchor(topAnchor, left: leftAnchor, bottom: bottomAnchor, right: rightAnchor, topConstant: 0, leftConstant: 12, bottomConstant: 0, rightConstant: 0, widthConstant: 0, heightConstant: 0)
+    }
+    
+    
+}
+
+
